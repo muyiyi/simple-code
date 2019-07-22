73d9e13ce5dfa3b46bed5d9eea6f96c7cf1ef40d
muyiyi <746088723@qq.com>
2019-07-22 20:19:16 +0800

    提交max-number-array.js

--- max-number-array.js
+++ max-number-array.js
@@ -1,8 +1,26 @@
 var max = 0;
-var a = [1,2312,1,33,44,5,2,12,2]
+var a = [1,2312,1,33,44,5,2,12,2];
 for (var i = 0; i < a.length; ++i) {
     if (a[i] > max) {
-        max = a[i]
+        max = a[i];
     }
 }
 console.log(max)
+
+
+
+
+
+function max(arr) {
+    var max = 0;
+    var i = 0;
+    while (i < arr.length) {
+        if (max < arr[i]) {
+            max = arr[i];  
+        }
+        ++i;
+    }
+    return max
+}
+max([1,2312,1,33,44,5,2,12,2])
+
