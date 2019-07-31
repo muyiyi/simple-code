37ac79e6cece067361bbb6494ff562fd5e4a2dca
muyiyi <746088723@qq.com>
2019-07-31 21:13:37 +0800

    提交sec-min.js

--- sec-min.js
+++ sec-min.js
@@ -1,9 +1,9 @@
 function sec(arr) {
     var min = arr[0];
     var sec = arr[1];
-    if (min < sec) {
-        min = arr[0];
-        sec = arr[1];
+    if (min > sec) {
+        min = arr[1];
+        sec = arr[0];
     }
     for (var i = 2; i < arr.length; ++i) {
         if (arr[i] < min) {
@@ -14,4 +14,4 @@ function sec(arr) {
     }
     return sec
 }
-sec([12, 89, 8, 32, 56, 30, 78])
+sec([12, 7, 8, 32, 56, 30, 78])
